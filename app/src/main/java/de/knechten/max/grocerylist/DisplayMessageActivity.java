a39c761160126b9b08b889538908beaecbff58cd
Maximilian Knechten <2346421+mkn1988@users.noreply.github.com>
2020-10-03 10:53:21 +0200

    Initial commit.

---
+++ app/src/main/java/de/knechten/max/grocerylist/DisplayMessageActivity.java
@@ -0,0 +1,14 @@
+package de.knechten.max.grocerylist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+    }
+}
