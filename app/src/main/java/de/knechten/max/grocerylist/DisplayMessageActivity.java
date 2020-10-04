8e5fe507aca3abc67c5160af046d1e0398f66778
Max Knechten <2346421+mkn1988@users.noreply.github.com>
2020-10-04 11:16:10 +0200

    feat: display the message

--- app/src/main/java/de/knechten/max/grocerylist/DisplayMessageActivity.java
+++ app/src/main/java/de/knechten/max/grocerylist/DisplayMessageActivity.java
@@ -1,8 +1,10 @@
 package de.knechten.max.grocerylist;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
     }
 }
